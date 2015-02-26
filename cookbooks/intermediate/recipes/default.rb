019328760806b2e1568d911b3520c74250d3d492
chuong.tran@offis.com.au <chuong.tran@offis.com.au>
2015-02-26 00:11:58 +0000

    Add chuongs special hellow world message to default.rb recipe

---
+++ cookbooks/intermediate/recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: intermediate
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+log "Hello World! You should try Chuongs Chef with RightScale!"
